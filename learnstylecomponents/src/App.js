43d17a461556512de4b6f2b1399f892bf1279667
ZWkang <907747874@qq.com>
2018-01-02 21:58:43 +0800

    update App.js
    update App.js

--- learnstylecomponents/src/App.js
+++ learnstylecomponents/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-
+// import Tip from './demo1.js'
+// import PassedProps from './demo2.js'
+// import ButtonGroupInJs from './demo3.js'
+// import StylingComponent from './demo4.js'
+// import ExtendStyleClass from './demo5.js'
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        {/* <Tip></Tip> */}
+        {/* <PassedProps></PassedProps> */}
+        {/* <ButtonGroupInJs></ButtonGroupInJs> */}
+        {/* <StylingComponent></StylingComponent> */}
+        {/* <ExtendStyleClass></ExtendStyleClass> */}
       </div>
     );
   }
